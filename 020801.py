c50c2dbd43afe67ed4bf33a512bf866b70657790
TiEM <a52675268@gmail.com>
2023-02-13 15:02:10 +0900

    알고리즘 개선
    개선은 성공했으나
    반례처리가 되지 않은 것으로 보임.
    일부 케이스만 통과하고 대다수의 케이스를 통과하지 못함

--- 020801.py
+++ 020801.py
@@ -7,23 +7,53 @@
 """
 
 
-def changNum(num,flag,flag0):
-    return num[:flag0] + '110' + num[flag0:flag] + num[flag+3:]
+# def changNum(num,flag,flag0):
+#     return num[:flag0] + '110' + num[flag0:flag] + num[flag+3:]
+
+# def findNum(num: int):
+#     flag = 0
+#     flag0 = -1
+#     while flag < (len(num)-2):
+#         print(num,flag,flag0)
+#         if num[flag:flag+3] == '110':
+#             num = changNum(num,flag,flag0+1)
+#             flag = flag0+2
+#             flag0 = flag
+#         elif num[flag] == '0':
+#             flag0 = flag
+#         flag += 1
+    
+#     return num
+
 
 def findNum(num: int):
+
+    #문자열을 돌면서 110을 찾고 제거하는 작업
     flag = 0
-    flag0 = -1
+    cnt = 0
     while flag < (len(num)-2):
-        print(num,flag,flag0)
         if num[flag:flag+3] == '110':
-            num = changNum(num,flag,flag0+1)
-            flag = flag0+2
-            flag0 = flag
-        elif num[flag] == '0':
-            flag0 = flag
+            cnt += 1
+            num = num[:flag] + num[flag+3:]
+            flag = max(0,flag-3)
+        flag += 1
+
+    #문자열을 돌면서 적절한 위치를 찾아 cnt만큼 110을 삽입
+    flag = 0
+    doubled = False
+    while flag < len(num):
+        if num[flag] == '1':
+            if doubled:
+                flag = max(0,flag-1)
+                break
+            else:
+                doubled = True
         flag += 1
     
+    num = num[:flag] + '110'*cnt + num[flag:]
     return num
+
+
 
 num = input()
 print(findNum(num))
