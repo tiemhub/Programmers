2fe08c24709f8abffaaad5c846c5c33477bf54f0
TiEM <a52675268@gmail.com>
2023-02-23 14:49:26 +0900

    알고리즘 구체화 필요
    bfs를 쓸지 백트래킹을 쓸지 고민해보고 알고리즘 구체화

--- 022101.py
+++ 022101.py
@@ -6,11 +6,18 @@
 bfs로 최댓값을 탐색?
 visited 배열을 만들어서 사용할 필요성
 
+bfs 탐색하면서 a,b가 동시에 같은 발판에 경우의 수를 고려해야함
+visited 데이터 갱신을 이동하는 즉시가 아닌, 이동 이후 자신이 있던 발판의 데이터를 바꾸는 식으로 구성
+
 """
 
 def solution(board, aloc, bloc):
     answer = -1
     
     visited = [False for _ in range(len(board[0]))]*len(board)
-    
+    visited[aloc[0],aloc[1]] = True
+    visited[bloc[0],bloc[1]] = True
+
+
+
     return answer
